1a4eb7a85d337206ea951f51c50fa0c3195767b7
ashiyaman <ashiya1005@gmail.com>
2025-03-07 16:52:24 +0530

    Done question 1

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('A4 and A5 JS Questions')
+
+function sumArray(numbersArray) {
+    let sum = 0;
+    for (let i = 0; i < numbersArray.length; i++) {
+      sum += numbers[i];
+    }
+    return sum;
+  }
+  const numbers = [5, 12, 7, 22, 35, 60, 42];
+  
+  console.log(sumArray(numbers));
+  
+
+  
